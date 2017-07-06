3e02999b269e74c5b04ef9d5de9db20e42e573e8
Huynh Tuan <huynh-t@pise.co.jp>
2017-07-06 17:29:22 +0900

    magic

---
+++ fire.js
@@ -0,0 +1,7 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyD2F7v9HaQvQQWNdY93Cw1HJ_2a3fXo4is",                             // Auth / General Use
+  authDomain: "mydesk-85a82.firebaseapp.com",         // Auth with popup/redirect
+  databaseURL: "https://mydesk-85a82.firebaseio.com", // Realtime Database
+  storageBucket: "gs://mydesk-85a82.appspot.com",          // Storage
+  messagingSenderId: "123456789"                  // Cloud Messaging
+});
